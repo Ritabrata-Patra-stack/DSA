3e909e07dae6d413ff0548cc646ab553640a5c6a
Ritabrata Patra <ritakol1234@gmail.com>
2026-07-29 16:13:08 +0530

    Time: 5 ms (80.77%) | Memory: 46.3 MB (6.18%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,23 +1,17 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String s = String.valueOf(x);
         int cpy = x, rev =0;
-        if(s.charAt(0) == s.charAt(s.length()-1))
-        {
         while(cpy != 0)
         {
             rev = 10 * rev+(cpy%10);
             cpy = cpy/10;
-        }
-        }
-        else
-        {
-            return false;
+            
         }
         if(rev == x && x>=0)
         {
             return true;
         }
+        
         return false;
     }
 }
