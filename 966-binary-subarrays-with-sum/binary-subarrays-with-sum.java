e46c464e1fd6a70a7093137b5cb291b1370e819e
Ritabrata Patra <ritakol1234@gmail.com>
2026-09-10 19:34:48 +0530

    Time: 1 ms (99.97%) | Memory: 50.7 MB (78.27%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,20 +1,38 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        int l = 0,sum = 0,a=0;
-        for(int i = 0;i<nums.length; i++)
-        {
-            sum = 0;
-            for(int j = i; j<nums.length; j++)
-            {
-                sum+= nums[j];
-                if(sum == goal)
-                {
-                    a++;
-                    
-                }
-            }
-        }
-        return a;
+        //int l = 0,sum = 0,a=0;
+        //for(int i = 0;i<nums.length; i++)
+        //{
+        //    sum = 0;
+        //    for(int j = i; j<nums.length; j++)
+        //    {
+        //        sum+= nums[j];
+        //        if(sum == goal)
+        //        {
+        //            a++;
+        //            
+        //        }
+        //    }
+        //}
+        //return a;
+        return atmost(nums,goal) - atmost(nums,goal-1);
         
+    }
+    public int atmost(int [] nums,int goal)
+    {
+        if(goal<0)
+        return 0;
+        int l = 0,count = 0, sum = 0;
+        for(int r = 0; r<nums.length; r++)
+        {
+            sum += nums[r];
+            while(sum>goal)
+            {
+                sum = sum-nums[l];
+                l++;
+            }
+            count += r-l+1;
+        }
+        return count;
     }
 }
